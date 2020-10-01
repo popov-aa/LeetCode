52f9006112e2595b8e6885131a2c6a955304e106
Popov Alexey <popov.spb@gmail.com>
2020-10-01 14:46:35 +0300

    2 Add Two Numbers optimization

--- 2 Add Two Numbers.cpp
+++ 2 Add Two Numbers.cpp
@@ -13,34 +13,42 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        return _addTwoNumbers(l1, l2, 0);
-    }
-
-    ListNode* _addTwoNumbers(ListNode* l1, ListNode* l2, int residual) {
-        ListNode * l = new ListNode(residual);
-        if (l1 != nullptr) {
-            l->val += l1->val;
-        }
-        if (l2 != nullptr) {
-            l->val += l2->val;
-        }
-        int _residual = 0;
-        if (l->val > 9) {
-            _residual = l->val / 10;
-            l->val %= 10;
-        }
-        ListNode* l1Next = nullptr;
-        if (l1) {
-            l1Next = l1->next;
-        }
-        ListNode* l2Next = nullptr;
-        if (l2) {
-            l2Next = l2->next;
-        }
-        if (l1Next != nullptr || l2Next != nullptr || _residual != 0) {
-            l->next = _addTwoNumbers(l1Next, l2Next, _residual);
-        }
-        return l;
+        ListNode * root = nullptr;
+        ListNode * prev = nullptr;
+        int residual = 0;
+        do {
+            ListNode * l = new ListNode(residual);
+            if (root == nullptr) {
+                root = l;
+            }
+            if (prev != nullptr) {
+                prev->next = l;
+            }
+            if (l1 != nullptr) {
+                l->val += l1->val;
+            }
+            if (l2 != nullptr) {
+                l->val += l2->val;
+            }
+            int _residual = 0;
+            if (l->val > 9) {
+                _residual = l->val / 10;
+                l->val %= 10;
+            }
+            ListNode* l1Next = nullptr;
+            if (l1) {
+                l1Next = l1->next;
+            }
+            ListNode* l2Next = nullptr;
+            if (l2) {
+                l2Next = l2->next;
+            }
+            l1 = l1Next;
+            l2 = l2Next;
+            residual = _residual;
+            prev = l;
+        } while (l1 != nullptr || l2 != nullptr || residual != 0);
+        return root;
     }
 };
 
